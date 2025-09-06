c972321bc099c67f9dbc31c3e990ca0917e36b27
Manmith7 <manmithgopari7@gmail.com>
2025-09-06 17:43:36 +0530

    fixed-errors

--- server.js
+++ server.js
@@ -9,25 +9,22 @@ import MappingRoutes from './routes/mappings.routes.js'
 import sequelize from './models/index.js'; 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.get('/', (req, res) => {
     res.send('API is running');
 });
 
-// Routes
 app.use('/api/auth', AuthRoutes);
 app.use('/api/patients', PatientRoutes);
 app.use('/api/doctors',DoctorRoutes)
 app.use('/api/mappings',MappingRoutes)
-// Test DB connection and sync tables
+
 const start = async () => {
     try {
         await sequelize.authenticate();
-        //console.log('PostgreSQL connected (Sequelize)');
-
-        await sequelize.sync({ alter: true }); // Creates/updates tables
-        //console.log('Tables synced');
+       
+        await sequelize.sync({ alter: true }); 
+       
 
         const PORT = process.env.PORT || 5000;
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
